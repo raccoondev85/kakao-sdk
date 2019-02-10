ed2b8b10913fd3dcf86cc72332fdd5b6d45ed57b
Jérémy LANDON <jeremy.landon@outlook.com>
2019-02-10 23:17:58 +0100

    set compatilibity with @ionic-native/core@5.x.x

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+import { Injectable } from '@angular/core';
+import { cordova, IonicNativePlugin } from '@ionic-native/core';
+
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -14,236 +20,101 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-import { Injectable } from '@angular/core';
-import { Cordova, IonicNativePlugin, Plugin } from '@ionic-native/core';
-var KakaoCordovaSDK = (function (_super) {
+
+var KakaoCordovaSDK = /** @class */ (function(_super) {
     __extends(KakaoCordovaSDK, _super);
     function KakaoCordovaSDK() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        return (_super !== null && _super.apply(this, arguments)) || this;
     }
-    KakaoCordovaSDK.prototype.login = function (loginOptions) {
-        return;
+    KakaoCordovaSDK.prototype.login = function(loginOptions) {
+        return cordova(this, 'login', {}, arguments);
     };
     KakaoCordovaSDK.prototype.logout = function () {
-        return;
+        return cordova(this, 'logout', {}, arguments);
     };
     KakaoCordovaSDK.prototype.unlinkApp = function () {
-        return;
+        return cordova(this, 'unlinkApp', {}, arguments);
     };
     KakaoCordovaSDK.prototype.getAccessToken = function () {
-        return;
+        return cordova(this, 'getAccessToken', {}, arguments);
     };
     KakaoCordovaSDK.prototype.requestMe = function () {
-        return;
+        return cordova(this, 'requestMe', {}, arguments);
     };
     KakaoCordovaSDK.prototype.updateScopes = function (targetScopes) {
-        return;
+        return cordova(this, 'updateScopes', {}, arguments);
     };
     KakaoCordovaSDK.prototype.checkScopeStatus = function (targetScopes) {
-        return;
+        return cordova(this, 'checkScopeStatus', {}, arguments);
     };
     KakaoCordovaSDK.prototype.requestSendMemo = function (builder) {
-        return;
+        return cordova(this, 'requestSendMemo', {}, arguments);
     };
     KakaoCordovaSDK.prototype.addPlusFriend = function (params) {
-        return;
+        return cordova(this, 'addPlusFriend', {}, arguments);
     };
     KakaoCordovaSDK.prototype.chatPlusFriend = function (params) {
-        return;
+        return cordova(this, 'chatPlusFriend', {}, arguments);
     };
     KakaoCordovaSDK.prototype.chatPlusFriendUrl = function (params) {
-        return;
+        return cordova(this, 'chatPlusFriendUrl', {}, arguments);
     };
     KakaoCordovaSDK.prototype.sendLinkFeed = function (feedTemplate) {
-        return;
+        return cordova(this, 'sendLinkFeed', {}, arguments);
     };
     KakaoCordovaSDK.prototype.sendLinkList = function (listTemplate) {
-        return;
+        return cordova(this, 'sendLinkList', {}, arguments);
     };
     KakaoCordovaSDK.prototype.sendLinkLocation = function (locationTemplate) {
-        return;
+        return cordova(this, 'sendLinkLocation', {}, arguments);
     };
     KakaoCordovaSDK.prototype.sendLinkCommerce = function (listTemplate) {
-        return;
+        return cordova(this, 'sendLinkCommerce', {}, arguments);
     };
     KakaoCordovaSDK.prototype.sendLinkText = function (textTemplate) {
-        return;
+        return cordova(this, 'sendLinkText', {}, arguments);
     };
     KakaoCordovaSDK.prototype.sendLinkScrap = function (scrapTemplate) {
-        return;
+        return cordova(this, 'sendLinkScrap', {}, arguments);
     };
     KakaoCordovaSDK.prototype.sendLinkCustom = function (customTemplate) {
-        return;
+        return cordova(this, 'sendLinkCustom', {}, arguments);
     };
     KakaoCordovaSDK.prototype.uploadImage = function (uploadImageConfig) {
-        return;
+        return cordova(this, 'uploadImage', {}, arguments);
     };
     KakaoCordovaSDK.prototype.deleteUploadedImage = function (deleteImageConfig) {
-        return;
+        return cordova(this, 'deleteUploadedImage', {}, arguments);
     };
     KakaoCordovaSDK.prototype.postStory = function (postStoryConfig) {
-        return;
+        return cordova(this, 'postStory', {}, arguments);
     };
-    KakaoCordovaSDK.decorators = [
-        { type: Injectable },
-    ];
-    /** @nocollapse */
-    KakaoCordovaSDK.ctorParameters = function () { return []; };
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "login", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", []),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "logout", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", []),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "unlinkApp", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", []),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "getAccessToken", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", []),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "requestMe", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "updateScopes", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "checkScopeStatus", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "requestSendMemo", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "addPlusFriend", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "chatPlusFriend", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "chatPlusFriendUrl", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "sendLinkFeed", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "sendLinkList", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "sendLinkLocation", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "sendLinkCommerce", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "sendLinkText", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "sendLinkScrap", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "sendLinkCustom", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "uploadImage", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "deleteUploadedImage", null);
-    __decorate([
-        Cordova(),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", Promise)
-    ], KakaoCordovaSDK.prototype, "postStory", null);
+    KakaoCordovaSDK.pluginName = 'Kakao Cordova SDK Plugin';
+    KakaoCordovaSDK.plugin = 'cordova-plugin-kakao-sdk';
+    KakaoCordovaSDK.pluginRef = 'KakaoCordovaSDK';
+    KakaoCordovaSDK.repo = 'https://github.com/raccoondev85/cordova-plugin-kakao-sdk';
+    KakaoCordovaSDK.platforms = ['Android', 'iOS'];
     KakaoCordovaSDK = __decorate([
-        Plugin({
-            pluginName: 'Kakao Cordova SDK Plugin',
-            plugin: 'cordova-plugin-kakao-sdk',
-            pluginRef: 'KakaoCordovaSDK',
-            repo: 'https://github.com/raccoondev85/cordova-plugin-kakao-sdk',
-            platforms: ['Android', 'iOS'],
-        })
+        Injectable()
     ], KakaoCordovaSDK);
     return KakaoCordovaSDK;
-}(IonicNativePlugin));
+})(IonicNativePlugin);
 export { KakaoCordovaSDK };
+
 export var AuthTypes;
-(function (AuthTypes) {
-    AuthTypes[AuthTypes["AuthTypeTalk"] = 1] = "AuthTypeTalk";
-    AuthTypes[AuthTypes["AuthTypeStory"] = 2] = "AuthTypeStory";
-    AuthTypes[AuthTypes["AuthTypeAccount"] = 3] = "AuthTypeAccount";
+(function(AuthTypes) {
+    AuthTypes[(AuthTypes['AuthTypeTalk'] = 1)] = 'AuthTypeTalk';
+    AuthTypes[(AuthTypes['AuthTypeStory'] = 2)] = 'AuthTypeStory';
+    AuthTypes[(AuthTypes['AuthTypeAccount'] = 3)] = 'AuthTypeAccount';
 })(AuthTypes || (AuthTypes = {}));
 export var ScrapType;
-(function (ScrapType) {
-    ScrapType[ScrapType["ScrapTypeNone"] = 0] = "ScrapTypeNone";
-    ScrapType[ScrapType["ScrapTypeWebsite"] = 1] = "ScrapTypeWebsite";
-    ScrapType[ScrapType["ScrapTypeVideo"] = 2] = "ScrapTypeVideo";
-    ScrapType[ScrapType["ScrapTypeMusic"] = 3] = "ScrapTypeMusic";
-    ScrapType[ScrapType["ScrapTypeBook"] = 4] = "ScrapTypeBook";
-    ScrapType[ScrapType["ScrapTypeArticle"] = 5] = "ScrapTypeArticle";
-    ScrapType[ScrapType["ScrapTypeProfile"] = 6] = "ScrapTypeProfile";
+(function(ScrapType) {
+    ScrapType[(ScrapType['ScrapTypeNone'] = 0)] = 'ScrapTypeNone';
+    ScrapType[(ScrapType['ScrapTypeWebsite'] = 1)] = 'ScrapTypeWebsite';
+    ScrapType[(ScrapType['ScrapTypeVideo'] = 2)] = 'ScrapTypeVideo';
+    ScrapType[(ScrapType['ScrapTypeMusic'] = 3)] = 'ScrapTypeMusic';
+    ScrapType[(ScrapType['ScrapTypeBook'] = 4)] = 'ScrapTypeBook';
+    ScrapType[(ScrapType['ScrapTypeArticle'] = 5)] = 'ScrapTypeArticle';
+    ScrapType[(ScrapType['ScrapTypeProfile'] = 6)] = 'ScrapTypeProfile';
 })(ScrapType || (ScrapType = {}));
 //# sourceMappingURL=index.js.map
